5d77d55db5776391607428be11c7a0bd718448c2
Byeon Ui-Hyeok <bym4094@naver.com>
2021-05-31 12:25:35 +0900

    내용 수정

--- Python 3/Number baseball/Number baseball 1.0.py
+++ Python 3/Number baseball/Number baseball 1.0.py
@@ -1,7 +1,7 @@
 import random
 filename_hidden = 0
 
-if filename_hidden == 0 : 
+if filename_hidden  == 0 : 
     filename_hidden = random.randint(100, 999)
     print(filename_hidden)
 else : 
@@ -16,27 +16,25 @@ filename_ball   = 0
 if filename_input [ 0 ] == filename_anwser [ 0 ]:
     filename_strike += 1 # 스트라이크
 elif filename_input [ 0 ] in filename_anwser :
-     filename_ball += 1 # 볼
+    filename_ball   += 1 # 볼
 # 두번째 숫자 확인
 if filename_input[1] == filename_anwser[1]:
     filename_strike += 1 # 스트라이크
 elif filename_input[1] in filename_anwser:
-    filename_ball += 1 # 볼
+    filename_ball   += 1 # 볼
 # 세번째 숫자 확인
 if filename_input[2] == filename_anwser[2]:
     filename_strike += 1 # 스트라이크
 elif filename_input[2] in filename_anwser:
-    filename_ball += 1 # 볼
+    filename_ball  += 1 # 볼
 if filename_strike == 0 and filename_ball == 0:
     print('아웃')
 else :
     
-    print('%sS %sB' % (filename_strike, filename_ball))
-    
-#    print( f'{filename_strike} + 'S' + {filename_ball} + 'B'')
+    print('%sS %sB' % (filename_strike, filename_ball))    
+#   print( f'{filename_strike} + 'S' + {filename_ball} + 'B'')
+#   print( '{0}' + 'S' + '{1}'+ 'B'.format(filename_strike, filename_ball))
 
-#    print( '{0}' + 'S' + '{1}'+ 'B'.format(filename_strike, filename_ball))
-
-# 수정요망
-# 반복문으로 묶을 수 있을지 포멧팅이용
+# 반복문으로 묶을 수 있을지?
+# 포멧팅 이용 가능한가?
 # 함수로 묶을 수 있을지.
